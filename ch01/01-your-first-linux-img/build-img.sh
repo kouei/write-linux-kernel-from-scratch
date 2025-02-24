46d033af41dacb7808108aece4facbd25c8a448a
kouei <kagigokoro@gmail.com>
2025-02-25 00:21:46 +0800

    Add --print-map to ld command

--- ch01/01-your-first-linux-img/build-img.sh
+++ ch01/01-your-first-linux-img/build-img.sh
@@ -2,4 +2,4 @@
 
 set -x # Trace commands being executed
 as bootsect.S -o bootsect.o
-ld bootsect.o -m elf_x86_64 --section-start=.text=0x0 --strip-all --oformat=binary --output=linux.img
+ld bootsect.o -m elf_x86_64 --section-start=.text=0x0 --strip-all --oformat=binary --output=linux.img --print-map
